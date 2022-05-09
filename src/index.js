8ad1c014018cfef639a57ba72c9c38681752c2ef
koba <koba@growal.com>
2022-05-09 13:34:59 +0000

    i

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 const component = () => {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('hello');
+
+  const icon = new Image();
+  icon.src = Icon;
+  element.appendChild(icon);
 
   return element;
 }
